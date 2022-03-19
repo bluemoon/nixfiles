a5433a413f8160443fbd6eab7e8662864e14dcca
Bradford Toney <bradford.toney@gmail.com>
2022-03-18 23:44:57 -0400

    fix: continuing

--- programs/neovim/lua/user/lsp/lsp-lua.lua
+++ programs/neovim/lua/user/lsp/lsp-lua.lua
@@ -1,16 +1,1 @@
-return {
-	settings = {
-
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
-}
+return require("lua-dev").setup({})
