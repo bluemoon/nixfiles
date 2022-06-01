af20ce3cd82b38a0a86dfeae922f34187ca79089
Bradford Toney <bradford.toney@gmail.com>
2022-06-01 15:47:51 -0400

    fix: add more

---
+++ modules/mac.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }: {
+
+nix = {
+  package = pkgs.nix;
+  extraOptions = ''
+    system = aarch64-darwin # M1 gang
+    extra-platforms = aarch64-darwin x86_64-darwin # But we use rosetta too
+    experimental-features = nix-command flakes
+    build-users-group = nixbld
+  '';
+};
+programs.fish.enable = true;
+environment.shells = with pkgs; [ fish ];
+}
