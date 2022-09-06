b8492d54066e209c6e8dc1594cd99c59afd3e298
Bradford Toney <bradford.toney@gmail.com>
2022-09-06 09:50:25 -0400

    fix: add modules

--- programs/neovim/lua/core/options.lua
+++ programs/neovim/lua/core/options.lua
@@ -1,30 +1,89 @@
-local o = vim.opt
+local opt = vim.opt
 
+-- Context [[ 
+opt.number = true -- bool: Show line numbers
+opt.relativenumber = true -- bool: Show relative line numbers
+opt.scrolloff = 4 -- int:  Min num lines of context
+opt.signcolumn = 'yes' -- str:  Show the sign column
+opt.wrap = false
+-- ]]
+
+-- Global Adjustments [[
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
+--vim.g.loaded_zipPlugin = 1
+--vim.g.loaded_zip = 1
 -- Leader
 vim.g.mapleader = ","
+-- ]]
 
-vim.opt.termguicolors = true
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.cmdheight = 2
-vim.opt.cursorline = true                       -- highlight the current line
-vim.opt.fileencoding = "utf-8"
-vim.opt.showtabline = 2                         -- always show tabs
-vim.opt.smartcase = true                        -- smart case
-vim.opt.smartindent = true                      -- make indenting smarter again
-vim.opt.number = true                           -- set number column width to 2 {default 4}
-vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.wrap = false                            -- wrap off
+-- [[ Theme ]]
+opt.syntax = 'ON' -- str:  Allow syntax highlighting
+opt.termguicolors = true -- bool: If term supports ui color then enable
+opt.background = 'dark'
 
--- Tabs
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.copyindent = true
+-- [[ Search ]]
+opt.ignorecase = true -- bool: Ignore case in search patterns
+opt.smartcase = true -- bool: Override ignorecase if search contains capitals
+opt.incsearch = true -- bool: Use incremental search
+opt.hlsearch = false -- bool: Highlight search matches
 
--- Remove builtin plugins
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_zip = 1
+-- [[ Whitespace ]]
+opt.expandtab = true -- bool: Use spaces instead of tabs
+opt.shiftwidth = 2 -- num:  Size of an indent
+opt.softtabstop = 2 -- num:  Number of spaces tabs count for in insert mode
+opt.tabstop = 2 -- num:  Number of spaces tabs count for
+opt.smarttab = true
+opt.smartindent = true
+opt.autoindent = true
+
+-- [[ Splits ]]
+opt.splitright = true -- bool: Place new window to right of current one
+opt.splitbelow = true -- bool: Place new window below the current one
+
+-- [[ Recovery ]]
+opt.undofile = true
+local cache_prefix = vim.fn.expand '~/.cache/nvim/'
+opt.undodir = cache_prefix .. '.undo/'
+opt.backupdir = cache_prefix .. '.backup/'
+opt.directory = cache_prefix .. '.swp/'
+
+opt.list = true
+opt.listchars:append 'space:⋅'
+opt.listchars:append 'eol:↴'
+
+opt.updatetime = 300
+opt.timeoutlen = 500
+
+opt.autochdir = true
+
+opt.mouse = 'a'
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+opt.pumblend = 10 -- Popup menu transparency
+opt.pumheight = 8 -- Popup menu height
+
+-- Shortmess
+opt.shortmess = opt.shortmess
+  + {
+    A = true, -- don't give the "ATTENTION" message when an existing swap file is found.
+    I = true, -- don't give the intro message when starting Vim |:intro|.
+    W = true, -- don't give "written" or "[w]" when writing a file
+    c = true, -- don't give |ins-completion-menu| messages
+    m = true, -- use "[+]" instead of "[Modified]"
+  }
+
+-- Format options
+opt.formatoptions = opt.formatoptions
+  + {
+    c = false,
+    o = false, -- O and o, don't continue comments
+    r = true, -- Pressing Enter will continue comments
+  }
