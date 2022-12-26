ad2bb9e93b58212799d93913cbb0998dc9a971fb
Bradford Toney <bradford.toney@gmail.com>
2022-12-26 00:41:52 -0500

    fix: other packages

--- programs/neovim/lua/modules/init.lua
+++ programs/neovim/lua/modules/init.lua
@@ -9,6 +9,113 @@ local plugins = {
     as = 'catppuccin',
     config = conf 'catppuccin',
   },
+  { -- Treesiter
+    'nvim-treesitter/nvim-treesitter',
+    config = conf 'nvim-treesitter',
+  },
+  { -- Finder
+    'nvim-telescope/telescope.nvim',
+    config = conf 'telescope',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      {
+        'ahmedkhalf/project.nvim',
+        config = function()
+          require('project_nvim').setup {}
+        end,
+      },
+    },
+  },
+  { -- Illuminate
+    'RRethy/vim-illuminate',
+  },
+  { -- Icons
+    'kyazdani42/nvim-web-devicons',
+    config = conf 'nvim-web-devicons',
+  },
+  { --Comment
+    'numToStr/Comment.nvim',
+    config = conf 'comment',
+    requires = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      config = conf 'context-commentstring',
+    },
+  },
+  { -- Lsp
+    'neovim/nvim-lspconfig',
+    config = conf 'lsp',
+    requires = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'jose-elias-alvarez/null-ls.nvim', -- Formatter
+      'ray-x/lsp_signature.nvim',
+      'jose-elias-alvarez/nvim-lsp-ts-utils',
+      'RRethy/vim-illuminate',
+      'simrat39/rust-tools.nvim',
+      {
+        'glepnir/lspsaga.nvim',
+        branch = 'main',
+        config = conf 'lspsaga',
+      },
+    },
+  },
+  { -- Autocompletion plugin
+    'hrsh7th/nvim-cmp',
+    config = conf 'nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-emoji',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-path',
+      'onsails/lspkind-nvim', -- Enables icons on completions
+      { -- Rust crates
+        'saecki/crates.nvim',
+        tag = 'v0.2.1',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function()
+          require('crates').setup()
+        end,
+      },
+      { -- Snippets
+        'L3MON4D3/LuaSnip',
+        requires = {
+          'saadparwaiz1/cmp_luasnip',
+          'rafamadriz/friendly-snippets',
+        },
+      },
+    },
+  },
+  { -- Git related
+    'lewis6991/gitsigns.nvim',
+    config = conf 'gitsigns',
+    requires = { 'nvim-lua/plenary.nvim' },
+  },
+  { -- Indent guides
+    'lukas-reineke/indent-blankline.nvim',
+    config = conf 'indent-blankline',
+  },
+  { -- Statusline
+    'feline-nvim/feline.nvim',
+    config = conf 'feline',
+  },
+  { -- Hop
+    'phaazon/hop.nvim',
+    config = conf 'hop',
+  },
+  { -- Highlight current line number
+    'yamatsum/nvim-cursorline',
+    config = conf 'nvim-cursorline',
+  },
+  { -- Autosave
+    'Pocco81/auto-save.nvim',
+    config = conf 'auto-save',
+  },
+  { -- Tree
+    'kyazdani42/nvim-tree.lua',
+    config = conf 'nvim-tree',
+  },
 }
 
 
