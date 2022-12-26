916193a0e972b80d8adbd14c2d13e5a74ca402fd
Bradford Toney <bradford.toney@gmail.com>
2022-12-26 00:55:43 -0500

    fix: update -> require

--- programs/neovim/lua/modules/config/lsp/handlers.lua
+++ programs/neovim/lua/modules/config/lsp/handlers.lua
@@ -82,7 +82,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 local cmp_nvim_lsp = safe_require 'cmp_nvim_lsp'
 if cmp_nvim_lsp then
-  capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+  capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
 M.capabilities = capabilities
 
