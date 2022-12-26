2113ffcf96693bdb2fa5ebb1598c8fc4ee803902
Bradford Toney <bradford.toney@gmail.com>
2022-12-26 13:56:25 -0500

    fix: formatting

--- programs/neovim/lua/core/keymaps.lua
+++ programs/neovim/lua/core/keymaps.lua
@@ -33,25 +33,29 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
   '',
   'f',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+  ,
   {}
 )
 vim.api.nvim_set_keymap(
   '',
   'F',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+  ,
   {}
 )
 vim.api.nvim_set_keymap(
   '',
   't',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
+  ,
   {}
 )
 vim.api.nvim_set_keymap(
   '',
   'T',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
+  ,
   {}
 )
 -- Code action
