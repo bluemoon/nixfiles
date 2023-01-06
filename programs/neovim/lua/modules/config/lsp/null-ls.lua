e499b5c8d748a9011abfee1d57bc43fb3489bf6c
Bradford Toney <bradford.toney@gmail.com>
2023-01-06 11:34:21 -0500

    fix: change config

--- programs/neovim/lua/modules/config/lsp/null-ls.lua
+++ programs/neovim/lua/modules/config/lsp/null-ls.lua
@@ -7,8 +7,21 @@ M.setup = function()
   end
 
   local format = null_ls.builtins.formatting
+  local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
   null_ls.setup {
+    on_attach = function(client, bufnr)
+      if client.supports_method 'textDocument/formatting' then
+        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { bufnr = bufnr }
+          end,
+        })
+      end
+    end,
     sources = {
       format.prettierd,
       format.stylua,
